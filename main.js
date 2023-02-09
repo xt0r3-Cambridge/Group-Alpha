5227d7424a629d0dd2c4b6ba9452105cf7b5c50a
samue <xemoryt@gmail.com>
2023-02-09 11:56:54 +0000

    made example scrape and classify functions and alerts if the page contains the phrase AI

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
-alert("among us")
-document.getElementById("answers").append("among us");
+(async () => {
+    const src = chrome.runtime.getURL("other.js");
+    const other = await import(src);
+    let paragraphs = document.getElementsByTagName("p");
+    let b = other.classify(other.scrape(document));
+    if (b) {
+        alert("AI!!!!!!")
+    }
+})();
+
+
+
