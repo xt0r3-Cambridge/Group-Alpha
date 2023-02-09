077756ffc0c6f2f9ea5d8168e61650b4de9494a5
Kornel Szabo <xt0r3@Kornel-laptop>
2023-02-09 12:11:35 +0000

    Add scraping method for scraping from websites

---
+++ scraping/main.py
@@ -0,0 +1,146 @@
+from time import sleep
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import nltk
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import json
+from argparse import ArgumentParser
+
+# Example URLs:
+# https://www.technologyreview.com/author/melissa-heikkila/
+# https://www.technologyreview.com/author/niall-firth/
+
+# Parsing functionality for command line arguments
+parser = ArgumentParser()
+parser.add_argument("-f", "--file", dest="filename",
+                    help="write scraped data to FILE in JSON format", metavar="FILE", required=False)
+parser.add_argument("-u", "--urls", dest="urls",
+                    help="The URLs you want to scrape the data from. At the moment, only technologyreview.com is supported.",
+                    metavar="url_1,url_2,...,url_n", required=True)
+parser.add_argument("-v", "--verbose", dest="verbose",
+                    help="Provide additional logging information", action='store_true')
+
+args = parser.parse_args()
+urls = [line.strip() for line in args.urls.split(",")]
+
+"""Print text only if the verbose flag is set. 
+    Defining the method this way ensures that verbosity won't be changed during the 
+    program runtime, even if the verbose flag gets changed somehow.
+"""
+verboseprint = print if args.verbose else lambda *a, **k: None
+
+def scraping(url):
+    """Scraping text from articles.
+
+    Scraping only html text and make them into word tokens.
+
+    Args:
+        url: the url of the target webpage
+
+    Returns:
+        A dict mapping keys to the corresponding list of tokens and list of list of tokens
+        for example:
+
+        {'subheadings': [['Subheading','One'],['Subheading','Two'],['Subheading','Three']],
+         'text': ['Hello','World','!','Hi','Python','!']}
+
+    Raises:
+        ValueError: An error occurred when a very weird url is given
+        URLError: An error occurred when the url can't be opened
+        HTTPError: An error occurred when the page is not found
+    """
+    pass
+
+    html = urlopen(url).read()
+    soup = BeautifulSoup(html, features="html.parser")
+
+    # get headlines
+    headline_list = []
+    heading_tags = ["h1", "h2", "h3"]
+    for tags in soup.find_all(heading_tags):
+        headline_list.append(nltk.word_tokenize(tags.text.strip()))
+
+    # kill all script and style elements
+    for script in soup(['script', 'style', 'h1', 'h2', 'h3', 'title']):
+        script.decompose()  # rip it out
+
+    # get text
+    text = ' '.join(soup.stripped_strings)
+
+    # break into lines and remove leading and trailing space on each
+    lines = (line.strip() for line in text.splitlines())
+    # break multi-headlines into a line each
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+    # drop blank lines
+    text = '\n'.join(chunk for chunk in chunks if chunk)
+
+    nltk_tokens = nltk.word_tokenize(text)
+
+    res = {"subheadings": headline_list, "text": nltk_tokens}
+    return res
+
+
+def scraping_urls_from_technology_review(url):
+    def scraping_text_from_technology_review(inner_urls):
+        html = urlopen(inner_urls).read()
+        soup = BeautifulSoup(html, features="html.parser")
+        # get text
+        ps = soup.find_all("p")
+        res = []
+        for p in ps:
+            res += nltk.word_tokenize(p.text)
+        return {"text": res}
+
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.implicitly_wait(26)
+    driver.get(url)
+
+    while 1:
+        try:
+            driver.find_element(By.ID, 'content-list__load-more-btn').click()
+            verboseprint("Loading news...")
+            sleep(0.5)
+        except:
+            verboseprint("Loaded all news")
+            
+            urls = []
+            headings = []
+            h3s = driver.find_elements(By.CLASS_NAME, "teaserItem__title--32O7a")
+            
+            verboseprint("Extracting headlines...")
+            
+            for x in h3s:
+                urls.append(x.find_element(By.TAG_NAME, "a").get_attribute("href"))
+                headings.append(x.text)
+                
+            verboseprint("Extracting text... This may take a while")
+
+            articles = []
+            for i in range(len(urls)):
+                articles.append(scraping_text_from_technology_review(urls[i]))
+                articles[i]['main_heading'] = headings[i]
+                
+            return articles
+
+
+'''
+The return format:
+    list of dictionaries, the keys in dictionary are main_heading and text
+    [{"main_heading":sth,
+      "text":[token1,token2...,token n]},
+      {"main_heading":sth,
+      "text":[token1,token2...,token n]},
+      ...]
+'''
+
+results = []
+
+for url in urls:
+    verboseprint(f"Processing {url}")
+    results.append(scraping_urls_from_technology_review(url))
+
+if args.filename:
+    with open(args.filename, "w") as f:
+        json.dump(results, f)
