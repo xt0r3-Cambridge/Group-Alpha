ba7b7de81e37696d9fc61bbc926cfa59ca9ca63e
yita <986983167@qq.com>
2023-02-09 11:50:48 +0000

    'scraper_in_js_is_added'

---
+++ scraping/scrape.js
@@ -0,0 +1,17 @@
+function getPTags() {
+  const elements = document.querySelectorAll("p");
+  var pTags = "";
+  for (const element of elements) {
+    pTags += (element.textContent.trim()+"\n");
+  }
+  return pTags;
+}
+
+function getTokenizedPTags() {
+  const elements = document.querySelectorAll("p");
+  const pTags = [];
+  for (const element of elements) {
+    pTags.push(element.textContent.trim().split(/\s+/));
+  }
+  return pTags;
+}
