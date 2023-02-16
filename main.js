39a2003c35347c8f85eaa928c270db90cca877a1
samue <xemoryt@gmail.com>
2023-02-16 11:06:56 +0000

    Made a function for loading the popup over a given webpage

--- main.js
+++ main.js
@@ -13,18 +13,6 @@ async function load_toolbar() {
     const other = await import(src);
     let paragraphs = document.getElementsByTagName("p");
     let b = other.classify(other.scrape(document));
-    /*
-    var wrapperIFrame = document.createElement("iframe"); 
-    wrapperIFrame.src = "chrome-extension://*";
-    wrapperIFrame.width = "640px";
-    wrapperIFrame.height = "480px";
-    var iframe = document.createElement("iframe");
-    iframe.setAttribute("src", chrome.runtime.getURL("html/card.html"));
-    iframe.style.width = "640px";
-    iframe.style.height = "480px";
-    wrapperIFrame.appendChild(iframe);
-    document.body.prepend(wrapperIFrame);
-    */
     if (b) {
         alert("AI!!!!!!");
     }
