6ee11d3c35f8c2723ef884805b8f311b1c2cc829
samue <xemoryt@gmail.com>
2023-02-16 12:03:11 +0000

    Renamed main.js to inject.js to more accurately represent its role in the project

--- inject.js
+++ inject.js
@@ -1,7 +1,6 @@
 async function load_toolbar() {
     let newElement = new DOMParser().parseFromString('<div id="toolbar"></div>', 'text/html').body.childNodes[0];
     let toolbar_url = chrome.runtime.getURL("html/card.html");
-  
     document.querySelector("body").appendChild(newElement);
     document.getElementById("toolbar").innerHTML = await (await fetch(toolbar_url)).text();
 }
@@ -9,13 +8,6 @@ async function load_toolbar() {
 
 (async () => {
     load_toolbar();
-    const src = chrome.runtime.getURL("other.js");
-    const other = await import(src);
-    let paragraphs = document.getElementsByTagName("p");
-    let b = other.classify(other.scrape(document));
-    if (b) {
-        alert("AI!!!!!!");
-    }
 })();
 
 
