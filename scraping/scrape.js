437d5b29d2c4fe34b3749e34ab57850bc18c3ed0
yita <986983167@qq.com>
2023-03-02 11:54:57 +0000

    'changes_in_scraper.js'

--- scraping/scrape.js
+++ scraping/scrape.js
@@ -1,17 +1,32 @@
 function getPTags() {
   const elements = document.querySelectorAll("p");
+
+  const h1 = document.querySelectorAll("h1");
+  const h2 = document.querySelectorAll("h2");
+  const h3 = document.querySelectorAll("h3");
+
+  hTags = h1.concat(h2,h3)
+
   var pTags = "";
   for (const element of elements) {
     pTags += (element.textContent.trim()+"\n");
   }
-  return pTags;
+
+  return (pTags, hTags);
 }
 
 function getTokenizedPTags() {
   const elements = document.querySelectorAll("p");
+
+  const h1 = document.querySelectorAll("h1");
+  const h2 = document.querySelectorAll("h2");
+  const h3 = document.querySelectorAll("h3");
+
+  hTags = h1.concat(h2,h3)
+
   const pTags = [];
   for (const element of elements) {
     pTags.push(element.textContent.trim().split(/\s+/));
   }
-  return pTags;
+  return (pTags, hTags);
 }
